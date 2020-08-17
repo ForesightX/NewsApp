16dd29d10111921c9ade5996c624c8d2cbcc85d3
Amuke Sunday <sundayamuke7@gmail.com>
2020-08-17 15:32:30 +0100

    Deleted the multiple fragments initially created

--- app/src/main/java/com/tenodevs/newsapp/adapters/NewsPagerAdapter.kt
+++ app/src/main/java/com/tenodevs/newsapp/adapters/NewsPagerAdapter.kt
@@ -1,5 +1,6 @@
 package com.tenodevs.newsapp.adapters
 
+import android.os.Bundle
 import androidx.fragment.app.*
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.tenodevs.newsapp.ui.*
@@ -9,13 +10,11 @@ class NewsPagerAdapter(fa: FragmentActivity, private val count: Int) : FragmentS
     override fun getItemCount() = count
 
     override fun createFragment(position: Int): Fragment {
-        return when (position) {
-            0 -> HeadlinesFragment()
-            1 -> BusinessFragment()
-            2 -> EntertainmentFragment()
-            3 -> HealthFragment()
-            else -> SportFragment()
+        val fragment = NewsFragment()
+        fragment.arguments = Bundle().apply {
+            putInt("POSITION", position)
         }
+        return fragment
     }
 }
 
