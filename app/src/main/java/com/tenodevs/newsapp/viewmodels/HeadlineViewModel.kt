c94b2f75e7f4fa9e5fbaeb6347e3a38fbf5f12e8
dev-juri <ojurifemi132@gmail.com>
2020-08-17 16:00:49 +0100

    Added logic for fetching news and filtering the news in the viewModel

--- app/src/main/java/com/tenodevs/newsapp/viewmodels/HeadlineViewModel.kt
+++ app/src/main/java/com/tenodevs/newsapp/viewmodels/HeadlineViewModel.kt
@@ -1,12 +1,16 @@
 package com.tenodevs.newsapp.viewmodels
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import android.util.Log
+import androidx.lifecycle.*
+import com.tenodevs.newsapp.domain.Article
+import com.tenodevs.newsapp.network.NewsAPI
+import com.tenodevs.newsapp.network.NewsFilter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class HeadlineViewModel(application: Application) : AndroidViewModel(application) {
     private val viewModelJob = SupervisorJob()
@@ -17,8 +21,33 @@ class HeadlineViewModel(application: Application) : AndroidViewModel(application
         viewModelJob.cancel()
     }
 
+    private val _newsList = MutableLiveData<List<Article>>()
+    val newsList: LiveData<List<Article>>
+        get() = _newsList
 
-    class HeadlineViewModelFactory(val application : Application): ViewModelProvider.Factory {
+    init {
+        getFilteredHeadlines(NewsFilter.HEADLINE)
+    }
+
+    private fun getFilteredHeadlines(category: NewsFilter) {
+        viewModelScope.launch {
+            val getNewsItems = NewsAPI.retrofitService.getLatestHeadlineAsync(category.value)
+
+            try {
+                val listResult = getNewsItems.await()
+                _newsList.value = listResult.articles
+                Log.i("FETCH_NEWS", "Success : ${listResult.totalResults} news fetched.")
+            } catch (e : HttpException) {
+                Log.e("FETCH_NEWS", "Failure: ${e.message()}")
+            }
+        }
+    }
+    fun updateNewsWithCategory(filter: NewsFilter) {
+        getFilteredHeadlines(filter)
+    }
+
+
+    class HeadlineViewModelFactory(val application: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(HeadlineViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
@@ -26,6 +55,5 @@ class HeadlineViewModel(application: Application) : AndroidViewModel(application
             }
             throw IllegalArgumentException("Unable to construct ViewModel")
         }
-
     }
 }
