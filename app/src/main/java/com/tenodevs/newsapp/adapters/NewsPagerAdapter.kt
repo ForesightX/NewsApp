077f01c6b32756f3f9b0ca603c577fd270873d96
Amuke Sunday <sundayamuke7@gmail.com>
2020-08-18 14:46:46 +0100

    Modified the NewsPagerAdapter.kt to return just one fragment
    The tab position is an argument

--- app/src/main/java/com/tenodevs/newsapp/adapters/NewsPagerAdapter.kt
+++ app/src/main/java/com/tenodevs/newsapp/adapters/NewsPagerAdapter.kt
@@ -1,21 +1,22 @@
 package com.tenodevs.newsapp.adapters
 
+import android.os.Bundle
 import androidx.fragment.app.*
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.tenodevs.newsapp.ui.*
+
+const val TAB_POSITION: String = "position"
 
 class NewsPagerAdapter(fa: FragmentActivity, private val count: Int) : FragmentStateAdapter(fa) {
 
     override fun getItemCount() = count
 
     override fun createFragment(position: Int): Fragment {
-        return when (position) {
-            0 -> HeadlinesFragment()
-            1 -> BusinessFragment()
-            2 -> EntertainmentFragment()
-            3 -> HealthFragment()
-            else -> SportFragment()
+        val fragment = HeadlinesFragment()
+        fragment.arguments = Bundle().apply {
+            putInt(TAB_POSITION, position)
         }
+        return fragment
     }
 }
 
