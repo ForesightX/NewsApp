6087e630fc6880177cfd36c0898e2fcee1eba44f
Amuke Sunday <sundayamuke7@gmail.com>
2020-08-19 17:41:01 +0100

    Annotated Article.kt as a Entity

--- app/src/main/java/com/tenodevs/newsapp/domain/Article.kt
+++ app/src/main/java/com/tenodevs/newsapp/domain/Article.kt
@@ -1,16 +1,16 @@
 package com.tenodevs.newsapp.domain
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@Entity
 data class Article(
     val author: String ?,
-    val content: String ?,
-    val description: String ?,
     val publishedAt: String ?,
-    val source: Source ?,
     val title: String ?,
-    val url: String,
+    @PrimaryKey val url: String,
     val urlToImage: String ?
 ) : Parcelable
