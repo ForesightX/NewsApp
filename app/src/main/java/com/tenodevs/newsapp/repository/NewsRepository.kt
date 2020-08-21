96aae6c90df5b9ec68516e1a86dc6771a530bd10
Amuke Sunday <sundayamuke7@gmail.com>
2020-08-21 15:42:34 +0100

    Implemented the use of SharedPreferences to save the time (as a Long) the news were last updated.
    This prevents the app from making unnecessary network calls.

--- app/src/main/java/com/tenodevs/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/tenodevs/newsapp/repository/NewsRepository.kt
@@ -1,6 +1,9 @@
 package com.tenodevs.newsapp.repository
 
 import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
@@ -13,6 +16,8 @@ import com.tenodevs.newsapp.viewmodels.Status
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.lang.Exception
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 class NewsRepository(
@@ -28,6 +33,31 @@ class NewsRepository(
     }
 
     suspend fun getFilteredHeadlines() {
+        // TODO: Use sharedPreferences
+        //  Check the time the news were last updated
+        //  If 0L is returned, this means the user is updating the news for the very first time
+        //  Else If the difference between the currentTime and the lastUpdateTime is less then 30 minutes, no need to make the network call
+        //  Else the cached news are outdated, and a new network call is required
+        val sharedPref = app.getSharedPreferences("LastUpdateTime", Context.MODE_PRIVATE)
+        val lastUpdateTime = sharedPref.getLong(category, 0L)
+
+        if (lastUpdateTime == 0L) {
+            loadFilteredHeadlines(sharedPref)
+        } else {
+            val currentTime = Date().time
+            if (currentTime.minus(lastUpdateTime) > TimeUnit.MINUTES.toMillis(30L)) {
+                loadFilteredHeadlines(sharedPref)
+
+                Log.i("NewsRepository by $category", "Made a network call")
+            } else {
+                // MAKE NO NETWORK CALL?
+                Log.i("NewsRepository by $category", "No need to make the network call")
+            }
+        }
+
+    }
+
+    private suspend fun loadFilteredHeadlines(sharedPref: SharedPreferences) {
         withContext(Dispatchers.IO) {
             val getNewsItems = NewsAPI.retrofitService.getLatestHeadlineAsync(category)
 
@@ -39,13 +69,12 @@ class NewsRepository(
                 val listDatabaseArticle = listResult.articles.toDatabaseModel(category)
 
                 _status.postValue(Status.DONE)
+                database.newsDao.deleteAll(category)
+                sharedPref.edit().putLong(category, Date().time).apply()
                 database.newsDao.insertAll(*listDatabaseArticle)
             } catch (e: Exception) {
-                withContext(Dispatchers.Main) {
-                    _status.value = Status.ERROR
-                }
+                _status.postValue(Status.ERROR)
             }
         }
     }
-
 }
