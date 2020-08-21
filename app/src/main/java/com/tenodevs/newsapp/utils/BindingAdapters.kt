f2d9250d5f6101d6f9bdb0c9c5a688be88d16486
Amuke Sunday <sundayamuke7@gmail.com>
2020-08-21 15:32:10 +0100

    Removed the binding adapter for the statusImageView

--- app/src/main/java/com/tenodevs/newsapp/utils/BindingAdapters.kt
+++ app/src/main/java/com/tenodevs/newsapp/utils/BindingAdapters.kt
@@ -1,8 +1,6 @@
 package com.tenodevs.newsapp.utils
 
-import android.view.View
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -10,7 +8,6 @@ import com.squareup.picasso.Picasso
 import com.tenodevs.newsapp.R
 import com.tenodevs.newsapp.adapters.NewsAdapter
 import com.tenodevs.newsapp.domain.Article
-import com.tenodevs.newsapp.viewmodels.Status
 
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
@@ -29,21 +26,4 @@ fun bindRecyclerView(recyclerView: RecyclerView, data: List<Article>?) {
     val adapter = recyclerView.adapter as NewsAdapter
 
     adapter.submitList(data)
-}
-
-@BindingAdapter("status")
-fun bindStatus(statusImageView: ImageView, status : Status ?) {
-    when(status) {
-        Status.LOADING -> {
-            statusImageView.visibility = View.VISIBLE
-            statusImageView.setImageResource(R.drawable.loading_animation)
-        }
-        Status.ERROR -> {
-            statusImageView.visibility = View.VISIBLE
-            statusImageView.setImageResource(R.drawable.ic_connection_error)
-        }
-        Status.DONE -> {
-            statusImageView.visibility = View.GONE
-        }
-    }
 }
